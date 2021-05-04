598b8f235209df1a7bf26c4858584d8e249e6911
Mehmet Eren Olgun <mehmeterenolgun@gmail.com>
2021-05-04 18:36:41 +0300

    twitter color theme added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const App = () => {
   return (
     <div>
-      <h1 className='bg-purple-500'>Twitter Clone</h1>
+      <h1 className='bg-primary-base'>Twitter Clone</h1>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          base: 'hsl(203, 89%, 53%)',
+          dark: 'hsl(203, 89%, 46%)',
+          light: 'hsl(203, 89%, 96%)',
+        },
+        gray: {
+          dark: '#657789',
+          light: '#aab8c2',
+          extraLight: '#e1e8ed',
+          lightest: '#f5f8fa',
+        },
+        black: '#14171a',
+      },
+    },
   },
   variants: {
     extend: {},

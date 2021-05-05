441eb871ef8ad5c9401efc724b7aaf452f3daf3b
Mehmet Eren Olgun <mehmeterenolgun@gmail.com>
2021-05-05 16:49:49 +0300

    icons added

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from 'react'
+import {
+  HomeIcon,
+  ExploreIcon,
+  NotificationsIcon,
+  MessagesIcon,
+  BookmarksIcon,
+  ListsIcon,
+  ProfileIcon,
+  MoreIcon,
+  PopulerIcon,
+  ImageIcon,
+  GIFIcon,
+  PollIcon,
+  EmojiIcon,
+  ScheduleIcon,
+  ReplyIcon,
+  ReTweetIcon,
+  LikeIcon,
+  ShareIcon,
+  SearchIcon
+} from '../icons/icon'
 
 const Sidebar = () => {
   return (
-    <div className='w-72 bg-blue-200'>
+    <div className="w-72 bg-blue-200">
       <h1>Sidebar</h1>
+      <HomeIcon />
+      <ExploreIcon />
+      <NotificationsIcon />
+      <MessagesIcon />
+      <BookmarksIcon />
+      <ListsIcon />
+      <ProfileIcon />
+      <MoreIcon />
+      <PopulerIcon className="w-7 h-7" />
+      <ImageIcon className="w-7 h-7" />
+      <GIFIcon className="w-7 h-7" />
+      <PollIcon className="w-7 h-7" />
+      <EmojiIcon className="w-7 h-7" />
+      <ScheduleIcon className="w-7 h-7" />
+      <ReplyIcon className="w-7 h-7" />
+      <ReTweetIcon className="w-7 h-7" />
+      <LikeIcon className="w-7 h-7" />
+      <ScheduleIcon className="w-7 h-7" />
+      <ShareIcon className="w-7 h-7" />
+      <SearchIcon className="w-7 h-7" />
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar

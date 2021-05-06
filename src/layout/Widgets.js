62c3ce1f0533f0784e0340dc6f199e02b90cfcad
Mehmet Eren Olgun <mehmeterenolgun@gmail.com>
2021-05-06 19:55:38 +0300

    timeline completed

--- src/layout/Widgets.js
+++ src/layout/Widgets.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from 'react'
+import { SearchIcon } from '../icons/Icon'
+
+import { Timeline } from 'react-twitter-widgets'
 
 const Widgets = () => {
   return (
-    <aside className='w-80 bg-purple-200'>
-      <h1>Timeline</h1>
+    <aside className="w-80">
+      <div className="flex items-center space-x-2 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base">
+        <SearchIcon className="w-5 h-5" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="placeholder-gray-dark bg-transparent outline-none w-full text-sm focus-within:text-black"
+        />
+      </div>
+      <div className="mt-5">
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'reactjs'
+          }}
+          options={{
+            height: '1000'
+          }}
+        />
+      </div>
     </aside>
-  );
-};
+  )
+}
 
-export default Widgets;
+export default Widgets
